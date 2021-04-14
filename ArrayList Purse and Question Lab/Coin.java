5cbe2f8958aa8f429334390dee7ff43aeabe9e0b
CalebYau <ccyau@stu.naperville203.org>
2021-04-14 12:39:25 -0500

    s

--- ArrayList Purse and Question Lab/Coin.java
+++ ArrayList Purse and Question Lab/Coin.java
@@ -1,49 +1,52 @@
 /**
 *  Edit class to fullfill Purse Requirements
 *
-*  Represents a coin with two sides that can be flipped.
+*  Represents a type of coin.
 */
 
-import java.util.Random;
+//import java.util.Random;
 
 public class Coin
 {
-   private final int HEADS = 0;
-   private final int TAILS = 1;
+   //private final int HEADS = 0;
+   //private final int TAILS = 1;
 
-   private int face;
-   private double amount;
-   private char type;
+   //private int face;
+   private int amount;
+   private String type;
+   
+   private int amountPenny, amountNickel, amountDime, amountQuarter = 0;
 
    /**-----------------------------------------------------------------
-   *  Sets up the coin by flipping it initially.
+   *  Sets up the coin by depending on the object.
    */
-   public Coin ()
+   public Coin (String type)
    {
-      flip();
+      type = type;
    }
-
-   /**-----------------------------------------------------------------
-   *  Flips the coin by randomly choosing a face value.
-   */
+   
+   /*
+   //-----------------------------------------------------------------
+   //Flips the coin by randomly choosing a face value.
+   //
    public void flip ()
    {
       face = (int) (Math.random() * 2);
    }
 
-   /**-----------------------------------------------------------------
-   *  Returns true if the current face of the coin is heads.
-   		@return boolean, true if heads
-   */
+   //-----------------------------------------------------------------
+   //Returns true if the current face of the coin is heads.
+   //@return boolean, true if heads
+   //
    public boolean isHeads ()
    {
       return (face == HEADS);
    }
 
-   /**-----------------------------------------------------------------
-   *  Returns the current face of the coin as a string.
-   		@return String
-   */
+   //-----------------------------------------------------------------
+   //Returns the current face of the coin as a string.
+   //@return String
+   //
    public String toString()
    {
       String faceName;
@@ -53,5 +56,36 @@ public class Coin
          faceName = "Tails";
 
       return faceName;
+   }
+   */
+   
+   public double getValue()
+   {
+       return (0.01 * amountPenny) + (0.05 * amountNickel) + (0.1 * amountDime) + (0.25 * amountQuarter);
+   }
+   
+   public void changeValue(int amount)
+   {
+       if (type.equals("P"))
+       {
+           amountPenny += amount;
+       }
+       else if (type.equals("N"))
+       {
+           amountNickel += amount;
+       }
+       else if (type.equals("D"))
+       {
+           amountDime += amount;
+       }
+       else
+       {
+           amountQuarter += amount;
+       }
+   }
+
+   public String getType()
+   {
+       return type;
    }
 }
--- ArrayList Purse and Question Lab/Purse.java
+++ ArrayList Purse and Question Lab/Purse.java
@@ -1,50 +1,115 @@
-
 import java.util.ArrayList;
 
 public class Purse
 {
-	//private ArrayList
-
-
-	public Purse (){ };
-
-	public Purse (Coin acoin)
+    //private ArrayList
+    ArrayList <Coin> change = new ArrayList();
+    
+    String type;
+    
+    public Purse (){};
+    
+    public Purse (Coin acoin)
+    {
+        add(acoin);
+    }
+    
+    /**
+     * precondition: Coin will be initialized (value and name) correctly
+     * each variable will have a value
+     * postcondition: Puts the Coin into the array list change
+     */
+    public void add (Coin acoin)
+    {
+        change.add(acoin);
+    }
+    
+    public void remove (String coin)
+    {
+        for (int i = 0; i < change.size(); i++)
+        {
+            if (change.get(i).getType() == coin)
+            {
+                change.remove(i);
+            }
+        }
+    }
+    
+    /**
+     * Returns the total value of the coins in the array
+     * list change
+     * @return ???
+     */
+    public double getTotal()
+    {
+        int total = 0;
+	    
+	for (Coin coin: change)
 	{
-		change.add(acoin);
+	    total += coin.getValue();
 	}
-  /** precondition: Coin will be initialized (value and name) correctly
-  *               each variable will have a value
-   	  postcondition: Puts the Coin into the array list change
-  */
-	public void add (Coin acoin)
-	{
-
-
-
-	}
-
-  /** Returns the total value of the coins in the array
-  *                 list change
-  		@return ???
-  */
-	public double getTotal()
-	{
-
-
-		return 0;
-
-	}
-
-
-
- /**
- *  Returns the all objects in the array list as Strings
- */
-
-   public String toString()
-   {
-      return " --";
-   }
-
+	    
+        return total;
+    }
+        
+    /**
+     * Returns the all objects in the array list as Strings
+     */
+    public String toString()
+    {
+        return getNumberofPenny() + "Pennies\t.01\n" + getNumberofNickels() + "Nickels\t.05\n" + getNumberofDimes() + "Dimes\t.10\n" + getNumberofQuarters() + "Quarters\t.25\n" + "Total " + getTotal();
+    }
+        
+    public int getNumberofDimes()
+    {
+        int number = 0;
+            
+        for (Coin coin: change)
+        {
+            if (coin.getType().equals("D"))
+                number += 1;
+        }
+            
+        return number;
+    }
+    
+    public int getNumberofPenny()
+    {
+        int number = 0;
+            
+        for (Coin coin: change)
+        {
+            if (coin.getType().equals("P"))
+                number += 1;
+        }
+            
+        return number;
+    }
+        
+    public int getNumberofNickels()
+    {
+        int number = 0;
+            
+        for (Coin coin: change)
+        {
+            if (coin.getType().equals("N"))
+                number += 1;
+        }
+            
+        return number;
+    }
+        
+    public int getNumberofQuarters()
+    {
+        int number = 0;
+            
+        for (Coin coin: change)
+        {
+            if (coin.getType().equals("Q"))
+                number += 1;
+        }
+            
+        return number;
+    }
 }
 //end of purse
